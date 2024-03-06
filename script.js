8eb80e0add2c51d05111a0b788651a1ad7603faf
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-06 10:30:41 -0800

    small changes

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+import { data } from "./data.js";
+
+// console.log(data);
 // Fetch the data using the following url
 const url = "https://restcountries.com/v3.1/all";
 
 // and answer the questions listed in the UI
-const q7 = document.querySelector("#q7 td:last-child");
+// const q7 = document.querySelector("#q7 td:last-child");
 
 // console.log(q7);
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const obj = {
-  name: "John Doe",
-  age: 32,
-  email: "johndoe@example.com",
-};
+// const obj = {
+//   name: "John Doe",
+//   age: 32,
+//   email: "johndoe@example.com",
+// };
 
-q7.innerHTML = `
-<pre>${JSON.stringify(obj, null, 2)}</pre>
-`;
+// q7.innerHTML = `
+// <pre>${JSON.stringify(obj, null, 2)}</pre>
+// `;
 
 // <pre>${JSON.stringify(array, null, 4)}</pre>
