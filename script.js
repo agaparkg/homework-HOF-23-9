1c2996da211adc0711718f8e34f6c956449165f4
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-06 10:26:10 -0800

    small changes

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 const url = "https://restcountries.com/v3.1/all";
 
 // and answer the questions listed in the UI
+const q7 = document.querySelector("#q7 td:last-child");
+
+// console.log(q7);
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const obj = {
+  name: "John Doe",
+  age: 32,
+  email: "johndoe@example.com",
+};
+
+q7.innerHTML = `
+<pre>${JSON.stringify(obj, null, 2)}</pre>
+`;
+
+// <pre>${JSON.stringify(array, null, 4)}</pre>
